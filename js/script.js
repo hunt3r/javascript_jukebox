929e268a6e213113830fdf564f3e3d1417d92649
Chris Hunter <chunter@urbn.com>
2016-11-24 23:08:17 -0500

    Add most of the functionality needed for playlist and some styles

--- js/script.js
+++ js/script.js
@@ -14,10 +14,29 @@
 // 4. Give the user the ability to load at least one different song into the Jukebox besides the one that is loaded when the page initially renders
 // The whole Jukebox should be backed by an object called Jukebox with methods to play, stop, and load songs.
 
+function formatPlayTime(seconds) {
+	var minutes = Math.floor(seconds / 60);
+	var remainingSeconds = Math.floor(seconds - (minutes * 60));
+	return remainingSeconds < 10 ? minutes + ':0' + remainingSeconds : minutes + ':' + remainingSeconds;
+}
+
+
+var eventPlaylistReload = new Event('PLAYLIST_RELOAD');
+
 var Song = function(src, artist, title) {
-	this.src = src;
-	this.artist = artist;
-	this.title = title;
+	var self = this;
+	self.src = src;
+	self.track = new Audio();
+	self.track.src = self.src;
+	self.duration = 0;
+	self.track.addEventListener('loadedmetadata', function(e) {
+		self.duration = formatPlayTime(this.duration);
+		document.dispatchEvent(eventPlaylistReload);
+	});
+	self.artist = artist;
+	self.title = title;
+	self.display = self.artist + " - " + self.title;
+	return self;
 };
 
 var Player = function() {
@@ -37,6 +56,7 @@ var Player = function() {
 	var duration = document.getElementById('duration');
 	var display = document.getElementById('controls');
 	var playlist = document.getElementById('playlist');
+	var displayCurrentTrack = document.getElementById('displayCurrentTrack');
 
 	function bindActions() {
 		playControl.addEventListener('click', self.playToggle);
@@ -47,9 +67,11 @@ var Player = function() {
 			self.nextSong();
 		});
 		self.audioPlayer.addEventListener('timeupdate', function(e) {
-			duration.innerText = Math.floor(this.duration);
-			currentTime.innerText = Math.floor(this.currentTime);
+			currentTime.innerText = formatPlayTime(this.currentTime);
 		})
+		document.addEventListener('PLAYLIST_RELOAD', function (e) {
+			self.reloadPlayList();
+		}, false);
 	};
 
 	self.addSong = function(song) {
@@ -57,27 +79,15 @@ var Player = function() {
 			self.currentTrackIndex = 0;
 		}
 		self.songs.push(song);
-		self.updatePlaylist(song, self.songs.length - 1);
-	};
-	self.play = function play() {
-		display.classList.remove('hidden');
-		self.audioPlayer.play();
-		playControl.innerText = 'Pause'
-		self.isPlaying = true;
-	} 
-	self.pause = function pause() {
-		self.audioPlayer.pause();
-		playControl.innerText = 'Play';
-		self.isPlaying = false;
-	}
-	self.playToggle = function(e) {
-		if (!self.isPlaying) {
-			self.play();
-		} else { 
-			self.pause();
-		}
+		self.createPlaylistItem(song, self.songs.length - 1);
 	};
 
+	self.getSongByIndex = function getSongByIndex(i) {
+		return self.songs[i];
+	}
+	self.getCurrentSong = function getCurrentSong() {
+		return self.songs[self.currentTrackIndex];
+	}
 	self.loadTrack = function(i) {
 		if (i > self.songs.length - 1) {
 			return;
@@ -87,13 +97,9 @@ var Player = function() {
 		if (self.isPlaying) {
 			self.audioPlayer.play();
 		}
-	};
-
-	self.stop = function(e) {
-		// tell player objet to stop
-		display.classList.add('hidden');
-		self.pause();
-		self.audioPlayer.src = self.songs[0].src;
+		displayCurrentTrack.innerText = self.getCurrentSong().display;
+		//Reload playlist for active states
+		document.dispatchEvent(eventPlaylistReload);
 	};
 
 	self.nextSong = function(e) {
@@ -105,6 +111,18 @@ var Player = function() {
 		self.loadTrack(self.currentTrackIndex);
 	};
 
+	self.pause = function pause() {
+		self.audioPlayer.pause();
+		playControl.innerText = 'Play';
+		self.isPlaying = false;
+	}
+
+	self.play = function play() {
+		self.audioPlayer.play();
+		playControl.innerText = 'Pause'
+		self.isPlaying = true;
+	} 
+
 	self.prevSong = function(e) {
 		self.currentTrackIndex -= 1;
 		if (self.currentTrackIndex < 0) {
@@ -114,20 +132,50 @@ var Player = function() {
 		self.loadTrack(self.currentTrackIndex);
 	};
 
-	self.updatePlaylist = function updatePlaylist(song, index) {
+	/* Toggles the player */
+	self.playToggle = function(e) {
+		if (!self.isPlaying) {
+			self.play();
+		} else { 
+			self.pause();
+		}
+	};
+
+	self.stop = function(e) {
+		// tell player objet to stop
+		self.pause();
+		self.loadTrack(0);
+	};
+
+	/** Add an item to the playlist */
+	self.createPlaylistItem = function createPlaylistItem(song, index) {
 		var item = document.createElement('li');
 		item.addEventListener('click', function(e) {
 			self.loadTrack(index);
 			if (!self.isPlaying) {
 				self.playToggle();
 			}
-		})
-		var text = document.createTextNode(song.artist + ' - ' + song.title);
+		});
+		item.id = index;
+		if (index == self.currentTrackIndex) {
+			item.classList.add('active')
+		}
+		var text = document.createTextNode(index + 1 + ' - ' + song.duration + ' - ' + song.artist + ' - ' + song.title );
 		item.appendChild(text);
 		playlist.appendChild(item);
 	
 	};
-	
+
+	/** Rebuild the playlist to handle changes */
+	self.reloadPlayList = function reloadPlayList() {
+		while (playlist.firstChild) {
+		    playlist.removeChild(playlist.firstChild);
+		}
+		for (var i=0; i < self.songs.length; i++) {
+			self.createPlaylistItem(self.songs[i], i);
+		}
+	}
+
 	bindActions();
 	return self;
 };
